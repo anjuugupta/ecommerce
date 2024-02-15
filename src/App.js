ecc11884fa829ccd9cac37c4d757bfa2a3725933
anjuugupta <128457053+anjuugupta@users.noreply.github.com>
2024-02-15 17:38:06 +0530

    ecom

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./customer/components/Nevigation/Nevigation";
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation></Navigation>
+    
+      <div>
+        <h1>E-Commerce</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
